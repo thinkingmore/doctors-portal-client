2b640d08d72a1a04549d73d563b98c09a5da3425
thinkingmore <peterkevin823@gmail.comm>
2022-11-17 20:12:00 +0600

    added dashboard layout component,available appointment display functionality

---
+++ src/Pages/Dashboard/MyAppointment/MyAppointment.js
@@ -0,0 +1,46 @@
+import { useQuery } from '@tanstack/react-query';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../../contexts/AuthProvider';
+
+const MyAppointment = () => {
+    const { user } = useContext(AuthContext);
+    
+    // const url = `http://localhost:5000/bookings?email=${user?.email}`
+    
+    const { data: bookings = [] } = useQuery({
+        queryKey: ['bookings', user?.email],
+        queryFn: () => fetch(`http://localhost:5000/bookings?email=${user?.email}`)
+        .then(res => res.json())
+    })
+    return (
+        <div>
+            <h3 className="text-3xl mb-5">My Appointment</h3>
+            <div className="overflow-x-auto">
+            <table className="table w-full">
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Name</th>
+                        <th>Treatment</th>
+                        <th>Date</th>
+                        <th>Time</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        bookings.map((booking, i) => <tr key={booking._id}>
+                            <th>{i+1}</th>
+                            <td>{booking.patient}</td>
+                            <td>{booking.treatment}</td>
+                            <td>{booking.appointmentDate}</td>
+                            <td>{booking.slot}</td>
+                        </tr>)
+                    }
+                </tbody>
+            </table>
+            </div>
+        </div>
+    );
+};
+
+export default MyAppointment;
--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,6 +1,8 @@
+import DashboardLayout from "../../Layout/DashboardLayout";
 import Main from "../../Layout/Main";
 import Appointment from "../../Pages/Appointment/Appointment/Appointment";
 import Dashboard from "../../Pages/Dashboard/Dashboard/Dashboard";
+import MyAppointment from "../../Pages/Dashboard/MyAppointment/MyAppointment";
 import Home from "../../Pages/Home/Home/Home";
 import Login from "../../Pages/Login/Login";
 import SignUp from "../../Pages/SignUp/SignUp";
@@ -34,7 +36,14 @@ const router = createBrowserRouter([
     },
     {
         path: '/dashboard',
-        element: <PrivateRoute><Dashboard></Dashboard></PrivateRoute>
+        element: <PrivateRoute><DashboardLayout></DashboardLayout></PrivateRoute>,
+        children: [
+            {
+
+                path: '/dashboard',
+                element: <MyAppointment></MyAppointment>
+            }
+        ]
     }
 ])
 
