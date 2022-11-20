9bd50bf69f9634cb1fabe694a52935e4008b71c2
thinkingmore <peterkevin823@gmail.comm>
2022-11-20 12:15:43 +0600

    set admin role for users and set show all users functionality only for admin

--- src/Layout/DashboardLayout.js
+++ src/Layout/DashboardLayout.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link, Outlet } from 'react-router-dom';
+import { AuthContext } from '../contexts/AuthProvider';
+import useAdmin from '../contexts/hooks/useAdmin';
 import Navbar from '../Pages/Shared/Navbar/Navbar';
 
 const DashboardLayout = () => {
+    const { user } = useContext(AuthContext);
+    const [isAdmin] = useAdmin(user?.email);
     return (
         <div>
             <Navbar></Navbar>
@@ -15,7 +19,11 @@ const DashboardLayout = () => {
                     <label htmlFor="dashboard-drawer" className="drawer-overlay"></label> 
                     <ul className="menu p-4 w-80 bg-base-100 text-base-content"> 
                         <li><Link to="/dashboard">My Appointments</Link></li>
-                        <li><Link to="/dashboard/allusers">All users</Link></li>
+                        {
+                            isAdmin && <>
+                                <li><Link to="/dashboard/allusers">All users</Link></li>
+                            </>
+                        }
                     </ul>           
                 </div>
                 </div> 
